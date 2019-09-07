f54d8d54a483442a02e6c73a408bf887b360b51e
marszm <mariusz.szmer@gmail.com>
2019-09-07 17:37:37 +0200

    inital app for BankingApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div><h1>{{pageTitle}}</h1>
+        <div>My First Component</div>
+  </div>`
 })
+
 export class AppComponent {
-  title = 'BankingApp by Mari000';
+  pageTitle : string  = 'dsadasda';
 }
