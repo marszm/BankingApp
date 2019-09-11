c4898091e13bb2d107620129a210642838623cad
marszm <mariusz.szmer@gmail.com>
2019-09-11 12:36:03 +0200

    security works fine

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
-import {Observable} from 'rxjs';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {AuthenticationService} from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(private authenticationService :AuthenticationService,
+              public router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return undefined;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    if(this.authenticationService.isUserLoggedIn())
+      return true;
+    this.router.navigate(['login']);
+    return false;
   }
 }
